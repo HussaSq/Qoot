50e1e08f37cd4244d19f1bf2f3a3dd6963f09428
fouzAlrabai <60447225+fouzAlrabai@users.noreply.github.com>
2020-02-05 10:45:08 +0300

    authentication of login and reset password are done

--- QootFolder/app/src/main/java/com/example/qootapplication/MainActivity.java
+++ QootFolder/app/src/main/java/com/example/qootapplication/MainActivity.java
@@ -1,18 +1,28 @@
-package com.example.qootapplication;  
-import androidx.appcompat.app.AppCompatActivity;  
-import android.content.Intent; 
-import android.os.Bundle; 
-import android.view.View; 
-import android.widget.Button;  
-public class MainActivity extends AppCompatActivity { 
-    Button b1; 
-     
-    protected void onCreate(Bundle savedInstanceState) { 
-        super.onCreate(savedInstanceState); 
-        setContentView(R.layout.activity_main);      } 
-//Just For Testing 
-// public void OpenReg(View view) {   
-// startActivity(new Intent(MainActivity.this,RegisterAsDonator .class));  
-// }// till here 
+package com.example.qootapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    //Just For Testing
+    public void OpenReg(View view) {
+
+
+        startActivity(new Intent(MainActivity.this,RegisterAsDonator .class));
+
+    }// till here
 }
 

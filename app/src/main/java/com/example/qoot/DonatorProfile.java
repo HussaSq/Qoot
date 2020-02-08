0a7868b83b6521774f3d12720b92d51eba95e871
AlharranAbeer <AlharranAbeer@gmail.com>
2020-02-08 15:26:31 +0300

    add spinner

--- app/src/main/java/com/example/qoot/DonatorProfile.java
+++ app/src/main/java/com/example/qoot/DonatorProfile.java
@@ -2,13 +2,58 @@ package com.example.qoot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import javax.annotation.Nullable;
 
 public class DonatorProfile extends AppCompatActivity {
+
+    private TextView Username;
+    private ImageView Photo;
+
+    // eventually we will add comments and ratings as well
+
+    FirebaseAuth mAuth ;
+    FirebaseFirestore fstore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donator_profile);
+
+        Username = findViewById(R.id.UserNameV);
+        Photo = findViewById(R.id.UserImage);
+
+        mAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+
+        String Uid = mAuth.getCurrentUser().getUid();
+        DocumentReference docRef = fstore.collection("users").document(Uid);
+        docRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                Username.setText(documentSnapshot.getString("username"));
+            }
+        });
+
+    }
+    public void OpenEditProfilePage(View view){
+        startActivity(new Intent(DonatorProfile.this,EditDonatorProfile.class));
+    }
+
+    public void OpenLogOut(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(DonatorProfile.this,LogIn.class));
     }
 }
--- app/src/main/java/com/example/qoot/EditVolunteerProfile.java
+++ app/src/main/java/com/example/qoot/EditVolunteerProfile.java
@@ -2,13 +2,28 @@ package com.example.qoot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class EditVolunteerProfile extends AppCompatActivity {
+
+    
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_volunteer_profile);
+
+
     }
+
+    public void OpenProfile(View view) {
+        startActivity(new Intent(EditVolunteerProfile.this,VolunteerProfile.class));
+    }
+
 }
+
+

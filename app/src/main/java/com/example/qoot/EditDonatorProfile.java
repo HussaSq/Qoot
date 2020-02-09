eb53fb6757dda426382c17d06afa7f6d13c3dbba
HussaSq <hessahsaud10@gmail.com>
2020-02-09 11:22:13 +0300

    Edit Done

--- app/src/main/java/com/example/qoot/EditDonatorProfile.java
+++ app/src/main/java/com/example/qoot/EditDonatorProfile.java
@@ -25,13 +25,14 @@ public class EditDonatorProfile extends AppCompatActivity {
 
     public ImageView NEW_IMAGE; // not important now
     private ImageView editIcon; // not important now
-
+    FirebaseFirestore db;
     private FirebaseFirestore fstore;
     private FirebaseAuth mAuth;
     DocumentReference Refrence;
+    String userId;
 
     // idk wth is this for
-    private static final String TAG = "";
+    private static final String TAG = "EditDonatorProfile";
 
     //  save button
     Button saveButton;
@@ -55,18 +56,20 @@ public class EditDonatorProfile extends AppCompatActivity {
             // firebase initialize
             mAuth = FirebaseAuth.getInstance();
             fstore =FirebaseFirestore.getInstance();
-
+            db=FirebaseFirestore.getInstance();
         Uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        userId=mAuth.getCurrentUser().getUid();
 
         //take from user
-        s1 = NEW_NAME.getText().toString();
-        s2 = NEW_PHONE.getText().toString();
+
 
             saveButton.setOnClickListener(new View.OnClickListener() {
 
                 @Override
                 public void onClick(View v) {
 
+                    s1 = NEW_NAME.getText().toString();
+                    s2 = NEW_PHONE.getText().toString();
                     //------ check name -------------
                     if (containsDigit(s1)) {
                         NEW_NAME.setError("Please enter a valid name with no numbers");
@@ -77,28 +80,46 @@ public class EditDonatorProfile extends AppCompatActivity {
                         return;
                     }
                     // ---------------- check number -------------
-                    if (containsLetters(s2)) {
-                        NEW_PHONE.setError("Please enter a valid phone number with no letters");
-                        return;
-                    }
-                    if (s2.length() < 10) {
-                        NEW_PHONE.setError("Please enter a valid phone length (10 Digits)");
-                        return;
-                    }
-                    if (!s2.startsWith("05")) {
-                        NEW_PHONE.setError("Please enter a valid phone length (Start with 05)");
-                        return;
-                    }
+                    if(!s2.isEmpty()) {
+                        s2 = NEW_PHONE.getText().toString();
+                        if (s2.length() > 10) {
+                            NEW_PHONE.setError("Please enter a valid phone length (10 Digits)");
+                            return;
+                        }
+                        s2 = NEW_PHONE.getText().toString();
 
+                        if (s2.length() < 10) {
+                            NEW_PHONE.setError("Please enter a valid phone length (10 Digits)");
+                            return;
+                        }
+
+                        s2 = NEW_PHONE.getText().toString();
+                        if (!s2.startsWith("05")) {
+                            NEW_PHONE.setError("Please enter a valid phone length (Start with 05)");
+                            return;
+                        }
+                        if (containsLetters(s2)) {
+                            NEW_PHONE.setError("Please enter a valid phone number with no letters");
+                            return;
+                        }
+                    }//end if empty
+                    s1 = NEW_NAME.getText().toString();
+                    s2 = NEW_PHONE.getText().toString();
                     // معليش على البدائيه بس اذا عندكم حل احسن قولو
+                    //حلك رائع بلا دراما:)
                     int counter=0;
                     if (Uid != null) {
-                        if (!(s1.isEmpty() || s1 == null))
-                            Updatename(s1);
+                        if (!(s1.isEmpty()) ){
+                            if (s1 != null)
+                                Updatename(s1);
+                        }
+
                         else
                             counter++;
-                        if (!(s2.isEmpty() || s2 == null))
-                            UpdatePhone(s2);
+                        if (!(s2.isEmpty())) {
+                            if (s2 != null)
+                                UpdatePhone(s2);
+                        }
                         else
                             counter++;
                         if (counter == 2)
@@ -120,44 +141,28 @@ public class EditDonatorProfile extends AppCompatActivity {
 
     public void Updatename(String name){
 
-        // same as firebase
-        Refrence = fstore.collection("users").document(Uid);
+        //MINE -Hussa
+        DocumentReference documentReference =db.collection("Donators").document(userId);
+        documentReference.update("UserName",NEW_NAME.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText( EditDonatorProfile.this,"user updated",Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        Refrence
-
-                .update("name", name)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "Name successfully updated!");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error updating Name", e);
-                    }
-                });
     }
     public void UpdatePhone (String phone){
 
-        Refrence = fstore.collection("users").document(Uid);
+        //Written by Hussa
+        DocumentReference documentReference =db.collection("Donators").document(userId);
+        documentReference.update("PhoneNumber",NEW_PHONE.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText( EditDonatorProfile.this,"user updated",Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        Refrence
 
-                .update("phone", phone)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "phone successfully updated!");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error updating Phone", e);
-                    }
-                });
     }
 
 
--- app/src/main/java/com/example/qoot/EditVolunteerProfile.java
+++ app/src/main/java/com/example/qoot/EditVolunteerProfile.java
@@ -6,11 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class EditVolunteerProfile extends AppCompatActivity {
 
     Spinner cars;
+    public EditText NEW_NAME;
+    public EditText NEW_PHONE;
+    FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+    String userId;
+
+
+    //  save button
+    Button saveButton;
+
+    // what user type in fields
+    String s1, s2, Uid;
 
 
     @Override
@@ -24,10 +45,148 @@ public class EditVolunteerProfile extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, types);
         cars.setAdapter(adapter);
         // I think here we need to fetch the type from DB.. not like the above
+
+
+        NEW_NAME = findViewById(R.id.Name);
+        NEW_PHONE = findViewById(R.id.Phone_v);
+
+
+        saveButton = findViewById(R.id.button);
+
+
+        // firebase initialize
+        mAuth = FirebaseAuth.getInstance();
+
+        db=FirebaseFirestore.getInstance();
+        Uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        userId=mAuth.getCurrentUser().getUid();
+
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //take from user
+                s1 = NEW_NAME.getText().toString();
+                s2 = NEW_PHONE.getText().toString();
+                //------ check name -------------
+                if (containsDigit(s1)) {
+                    NEW_NAME.setError("Please enter a valid name with no numbers");
+                    return;
+                }
+                if (s1.length() == 1) {
+                    NEW_NAME.setError("Please enter a valid name length");
+                    return;
+                }
+                // ---------------- check number -------------
+                if(!s2.isEmpty()) {
+                    s2 = NEW_PHONE.getText().toString();
+                    if (s2.length() > 10) {
+                        NEW_PHONE.setError("Please enter a valid phone length (10 Digits)");
+                        return;
+                    }
+                    s2 = NEW_PHONE.getText().toString();
+
+                    if (s2.length() < 10) {
+                        NEW_PHONE.setError("Please enter a valid phone length (10 Digits)");
+                        return;
+                    }
+
+                    s2 = NEW_PHONE.getText().toString();
+                    if (!s2.startsWith("05")) {
+                        NEW_PHONE.setError("Please enter a valid phone length (Start with 05)");
+                        return;
+                    }
+                    if (containsLetters(s2)) {
+                        NEW_PHONE.setError("Please enter a valid phone number with no letters");
+                        return;
+                    }
+                }//end if empty
+                s1 = NEW_NAME.getText().toString();
+                s2 = NEW_PHONE.getText().toString();
+                // معليش على البدائيه بس اذا عندكم حل احسن قولو
+                //حلك رائع بلا دراما:)
+                int counter=0;
+                if (Uid != null) {
+                    if (!(s1.isEmpty()) ){
+                        if (s1 != null)
+                            Updatename(s1);
+                    }
+
+                    else
+                        counter++;
+                    if (!(s2.isEmpty())) {
+                        if (s2 != null)
+                            UpdatePhone(s2);
+                    }
+                    else
+                        counter++;
+                    if (counter == 2)
+                    {
+                        Toast.makeText(EditVolunteerProfile.this, "No Changes on profile", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(EditVolunteerProfile.this, VolunteerProfile.class));
+                    }
+                    Toast.makeText(EditVolunteerProfile.this, "Changes Saved successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(EditVolunteerProfile.this, VolunteerProfile.class));
+                }
+            }
+        });
     }
 
     public void OpenProfile(View view) {
         startActivity(new Intent(EditVolunteerProfile.this,VolunteerProfile.class));
+    }
+    public void Updatename(String name){
+
+        //MINE -Hussa
+        DocumentReference documentReference =db.collection("Volunteers").document(userId);
+        documentReference.update("UserName",NEW_NAME.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText( EditVolunteerProfile.this,"user updated",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    public void UpdatePhone (String phone){
+
+        //Written by Hussa
+        DocumentReference documentReference =db.collection("Volunteers").document(userId);
+        documentReference.update("PhoneNumber",NEW_PHONE.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText( EditVolunteerProfile.this,"user updated",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // extra methods for checking
+    public final boolean containsDigit(String s) {
+        boolean containsDigit = false;
+
+        if (s != null && !s.isEmpty()) {
+            for (char c : s.toCharArray()) {
+                if (containsDigit = Character.isDigit(c)) {
+                    break;
+                }
+            }
+        }
+
+        return containsDigit;
+    }
+    public final boolean containsLetters(String s) {
+        boolean containsLetters = false;
+
+        if (s != null && !s.isEmpty()) {
+            for (char c : s.toCharArray()) {
+                if (containsLetters = Character.isLetter(c)) {
+                    break;
+                }
+            }
+        }
+
+        return containsLetters;
     }
 
 }

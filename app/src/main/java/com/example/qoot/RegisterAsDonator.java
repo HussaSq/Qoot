af10c4987b5a86f6ffd30aa8c7cade034e866dbf
HussaSq <hessahsaud10@gmail.com>
2020-02-16 20:01:49 +0300

    verify email

--- app/src/main/java/com/example/qoot/RegisterAsDonator.java
+++ app/src/main/java/com/example/qoot/RegisterAsDonator.java
@@ -20,6 +20,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -96,6 +97,20 @@ public class RegisterAsDonator extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            // تشيك للايميل اذا صدقي او لا
+                            FirebaseUser fuser = mAuth.getCurrentUser();
+                            fuser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(RegisterAsDonator.this, "Verification Email Has Been Sent ", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG,"OnFailure: Email Not Sent"+e.getMessage());
+                                }
+                            });
+
                             userId = mAuth.getCurrentUser().getUid();
                             Toast.makeText(RegisterAsDonator.this, "Registration Was Successful!!", Toast.LENGTH_SHORT).show();
 

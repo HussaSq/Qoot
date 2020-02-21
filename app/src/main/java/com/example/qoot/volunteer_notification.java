7a92a71f966cbd5568855b4c6d99a9a46e2f54cc
hadeel-ahm <59453031+hadeel-ahm@users.noreply.github.com>
2020-02-22 01:47:49 +0300

    nav_volenteer

--- app/src/main/java/com/example/qoot/volunteer_notification.java
+++ app/src/main/java/com/example/qoot/volunteer_notification.java
@@ -6,34 +6,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class VolunteerRequests extends AppCompatActivity {
+public class volunteer_notification extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_volunteer_requests);
-
+        setContentView(R.layout.activity_volunteer_notification);
         BottomNavigationView bottomNavigationView =findViewById(R.id.bottom_navigation_don);
-        bottomNavigationView.setSelectedItemId(R.id.Req_don);
+        bottomNavigationView.setSelectedItemId(R.id.notifi_don);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()){
-                    case R.id.notifi_don:
-                        startActivity(new Intent(getApplicationContext(),volunteer_notification.class));
+                    case R.id.prfile_don:
+                        startActivity(new Intent(getApplicationContext(),VolunteerProfile.class));
                         overridePendingTransition(0,0);
                         return true;
 
-                    case R.id.Req_don:
+                    case R.id.notifi_don:
                         return true;
 
-                    case R.id.prfile_don:
-                        startActivity(new Intent(getApplicationContext(),VolunteerProfile.class));
+                    case R.id.Req_don:
+                        startActivity(new Intent(getApplicationContext(),VolunteerRequests.class));
                         overridePendingTransition(0,0);
                         return true;
 
@@ -43,16 +41,5 @@ public class VolunteerRequests extends AppCompatActivity {
         });
 
 
-
-
-
-    }
-
-    public void OpenVolunteerRequestInfo(View view) {
-        startActivity(new Intent(VolunteerRequests.this,VolunteerRequestInfo.class));
-    }
-
-    public void OpenAllRequests(View view) {
-        startActivity(new Intent(VolunteerRequests.this,AllRequests.class));
     }
 }

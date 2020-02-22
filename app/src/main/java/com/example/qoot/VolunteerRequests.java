a2ca8648d1f7c5e79f9c9a428de401f1ba43d4be
AlharranAbeer <AlharranAbeer@gmail.com>
2020-02-23 01:02:29 +0300

    requests for vol and donator

--- app/src/main/java/com/example/qoot/VolunteerRequests.java
+++ app/src/main/java/com/example/qoot/VolunteerRequests.java
@@ -1,5 +1,6 @@
 package com.example.qoot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,27 +8,69 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class VolunteerRequests extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
+    LinearLayout req1 ;
+    RelativeLayout whole;
+    String VolunteerID;
+    TextView no;
+
+    /*
+
+                                فولنتير سوا اكسبت
+
+
+
+     */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volunteer_requests);
+            whole = findViewById(R.id.Parent);
+            req1 = findViewById(R.id.req1);
+            whole.removeView(req1);
+            no = findViewById(R.id.NO);
 
+            mAuth = FirebaseAuth.getInstance();
+            db = FirebaseFirestore.getInstance();
+            VolunteerID = mAuth.getCurrentUser().getUid();
 
-        // if this exact volunteer mAuth.getCurrentUser().getUId();   accepted the request
-        // we add this accepted request to his/her list.
+        Query q1 = db.collection("Requests").whereEqualTo("VolnteerID",VolunteerID);
 
-
-
-
+        q1.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String State = document.getString("State");
+                                String Event = document.getString("TypeOfEvent");
+                                NewRequestXML(Event,State);
+                            }
+                        } else {
+                           // Log.d(TAG, "Error getting documents: ", task.getException());
+                           no.setVisibility(View.VISIBLE);
+                        }
+                    }
+                });
     }
 
-
-
-    public void NewRequestXML(){
+    public void NewRequestXML(String Event, String status){
 
         // this is the bigger request layout ((Root))
         LinearLayout parent = new LinearLayout(this);
@@ -39,7 +82,7 @@ public class VolunteerRequests extends AppCompatActivity {
         parent.setClickable(true);
         //add the children of this parent or root
         TextView EventType  =  new TextView(this);
-        EventType.setText("EventType");
+        EventType.setText(Event);
         EventType.setLayoutParams(new LinearLayout.LayoutParams(199,40 ));
         EventType.setPadding(30,20,0,0);
         EventType.setTextSize(22);
@@ -48,17 +91,17 @@ public class VolunteerRequests extends AppCompatActivity {
         Status.setLayoutParams(new LinearLayout.LayoutParams(199,40));
         EventType.setPadding(30,5,0,0);
         EventType.setTextSize(22);
-        EventType.setText("Status");
+        EventType.setText(status);
         //--------------------------------------------------------------------
         ImageView urgentIcon = new ImageView(this);
         urgentIcon.setLayoutParams(new LinearLayout.LayoutParams(50,50));
         urgentIcon.setPadding(70,8,0,0);
-        //urgentIcon.set();
-        //  android:src="@drawable/urgent" />
+        urgentIcon.setImageResource(R.drawable.urgent);
 
-
+        parent.addView(EventType);
+        parent.addView(Status);
+        parent.addView(urgentIcon);
     }
-
 
     public void OpenVolunteerRequestInfo(View view) {
         startActivity(new Intent(VolunteerRequests.this,VolunteerRequestInfo.class));

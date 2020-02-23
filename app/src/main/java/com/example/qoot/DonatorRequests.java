2903353768b038e57a9a0138a689d9a0f1106856
AlharranAbeer <AlharranAbeer@gmail.com>
2020-02-23 18:34:19 +0300

    commit

--- app/src/main/java/com/example/qoot/DonatorRequests.java
+++ app/src/main/java/com/example/qoot/DonatorRequests.java
@@ -75,15 +75,12 @@ public class DonatorRequests extends AppCompatActivity {
                         startActivity(new Intent(getApplicationContext(),DonatorNotifications.class));
                         overridePendingTransition(0,0);
                         return false;
-
                     case R.id.prfile_don:
                         startActivity(new Intent(getApplicationContext(),DonatorProfile.class));
                         overridePendingTransition(0,0);
                         return false;
-
                     case R.id.Req_don:
                         return true;
-
                 }
                 return false;
             }
@@ -223,6 +220,7 @@ public void NewRequestXML(String Event, String Time, RelativeLayout whole, Strin
     public void OpenDonaterRequestInfo(View view) {
 
 
+
         Intent intent = new Intent(DonatorRequests.this,DonatorRequestInfo.class);
         intent.putExtra("RequestID",reqID);
         //if (reqID!= null)
@@ -230,6 +228,9 @@ public void NewRequestXML(String Event, String Time, RelativeLayout whole, Strin
             Toast.makeText(DonatorRequests.this, "It "+reqID, Toast.LENGTH_SHORT).show();
         //}
         startActivity(intent);
+
+        startActivity(new Intent(DonatorRequests.this,DonatorRequestInfo.class));
+
     }
 }
 

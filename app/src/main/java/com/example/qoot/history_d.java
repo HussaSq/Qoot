10551c943e0b4a07ae9849f7236eaf45e35977b9
hadeel-ahm <59453031+hadeel-ahm@users.noreply.github.com>
2020-04-11 13:09:23 +0300

    still"ordring reviews" :)

--- app/src/main/java/com/example/qoot/history_d.java
+++ app/src/main/java/com/example/qoot/history_d.java
@@ -6,16 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -23,65 +21,37 @@ import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.security.PublicKey;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import android.widget.AdapterView;
-
-import static com.google.firebase.firestore.Query.Direction.DESCENDING;
-
-
-public class DonatorRequests extends AppCompatActivity {
-
-
+public class history_d extends AppCompatActivity {
     FirebaseAuth mAuth;
     FirebaseFirestore db;
     String UserID,reqID;
     Request MAGIC;
     ListView listView;
+
+
     ArrayList<Request> request;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_donator_requests);
-        listView=findViewById(R.id.list_Request);
+        setContentView(R.layout.activity_history_d);
+        listView=findViewById(R.id.hisDList);
         request=new ArrayList<Request>();
+
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
         UserID = mAuth.getCurrentUser().getUid();
 
-        BottomNavigationView bottomNavigationView =findViewById(R.id.bottom_navigation_don);
-        bottomNavigationView.setSelectedItemId(R.id.Req_don);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.notifi_don:
-                        startActivity(new Intent(getApplicationContext(),DonatorNotifications.class));
-                        overridePendingTransition(0,0);
-                        return false;
-
-                    case R.id.prfile_don:
-                        startActivity(new Intent(getApplicationContext(),DonatorProfile.class));
-                        overridePendingTransition(0,0);
-                        return false;
-
-                    case R.id.Req_don:
-                        return true;
-                }
-                return false;
-            }
-        });
-        Query q1 = db.collection("Requests").whereEqualTo("DonatorID",UserID).whereIn("State", Arrays.asList("Pending","Accepted"));
-
+        Query q1 = db.collection("Requests").whereEqualTo("DonatorID",UserID).whereIn("State", Arrays.asList("Cancelled","Delivered"));
         q1.get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -96,7 +66,7 @@ public class DonatorRequests extends AppCompatActivity {
                                 String VolunteerName=document.getString("VolnteerName");
                                 MAGIC= new Request(Event, State, mAuth.getCurrentUser().getUid(), reqID, REQTYPE,DonatorName,VolunteerName);
                                 request.add(MAGIC);
-                                MyRequestAdapter myRequestAdapter=new MyRequestAdapter(DonatorRequests.this,R.layout.activity_single_request,request);
+                                MyRequestAdapter1 myRequestAdapter=new MyRequestAdapter1(history_d.this,R.layout.single_his,request);
                                 listView.setAdapter(myRequestAdapter);
                                 listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                                     @Override
@@ -104,7 +74,7 @@ public class DonatorRequests extends AppCompatActivity {
                                         Request temp = (Request) parent.getItemAtPosition(position);
                                         Intent in = getIntent();
                                         in.putExtra("RequestID",temp.getID());
-                                        Intent intent = new Intent(DonatorRequests.this,DonatorRequestInfo.class);
+                                        Intent intent = new Intent(history_d.this,DonatorRequestInfo.class);
                                         intent.putExtra("RequestID",in.getStringExtra("RequestID"));
                                         startActivity(intent);
                                     }
@@ -118,150 +88,30 @@ public class DonatorRequests extends AppCompatActivity {
                 });
 
     }
-
-    public void OpenRequestForm(View view) {
-        Intent intent = new Intent(DonatorRequests.this,requestForm.class);
-        startActivity(intent);
-        // startActivity(new Intent(DonatorRequests.this,requestForm.class));
+    public void opentDonatorRequset(View view){
+        startActivity(new Intent(history_d.this,DonatorRequests.class));
     }
-
-    public void OpenDonatorHis(View view) {
-        Intent intent = new Intent(DonatorRequests.this,history_d.class);
-        startActivity(intent);
-
-    }
-    // ------   SAME GOES HERE -------
-    //public void OpenDonaterRequestInfo(View view) {
-    //  Intent intent = new Intent(DonatorRequests.this,DonatorRequestInfo.class);
-    // intent.putExtra("RequestID",reqID);
-    //startActivity(intent);
-    //}
 }
 
-class Request {
-
-    public String EventType;
-    public String Status;
-    public String UserID;
-    public String ID;
-    public String type;
-    public String numOfGuest;
-    public String time;
-    public String date;
-    public String DonatorName;
-    public String VolunteerName;
-
-    public Uri getPicture() {
-        return picture;
-    }
-
-    public void setPicture(Uri picture) {
-        this.picture = picture;
-    }
-
-    public Uri picture;
 
 
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-
-
-    public Request(){
-
-    }
-
-    public Request(String type, String stat, String id, String reqID, String typeOfReq,String DonatorName,String VolunteerName){
-        EventType = type;
-        Status = stat;
-        UserID = id;
-        ID = reqID;
-        this.type =typeOfReq;
-        this.DonatorName=DonatorName;
-        this.VolunteerName=VolunteerName;
-    }
-
-    public String getID() {
-        return ID;
-    }
-
-    public void setID(String ID) {
-        this.ID = ID;
-    }
-
-    public String getEventType() {
-        return EventType;
-    }
-
-    public void setEventType(String eventType) {
-        EventType = eventType;
-    }
-
-    public String getVolunteerName() {
-        return VolunteerName;
-    }
-
-    public void setVolunteerName(String volunteerName) {
-        VolunteerName = VolunteerName;
-    }
-
-    public String getDonatorName() {
-        return DonatorName;
-    }
-
-    public void setDonatorName(String DonatorName) {
-        this.DonatorName = DonatorName;
-    }
-
-    public String getStatus() {
-        return Status;
-    }
-
-    public void setStatus(String status) {
-        Status = status;
-    }
-
-    public String getUserID() {
-        return UserID;
-    }
-
-    public void setUserID(String userID) {
-        UserID = userID;
-    }
-
-    public String getTypeOfReq() {
-        return type;
-    }
-
-    public void setTypeOfReq(String type) {
-        this.type = type;
-    }
-
-
-}
-
-class MyRequestAdapter extends BaseAdapter{
+class MyRequestAdapter1 extends BaseAdapter {
 
     private Context context;
     ArrayList<Request> request;
     int layoutResourseId;
 
 
-    MyRequestAdapter(Context context,ArrayList<Request> request){
+    MyRequestAdapter1(Context context,ArrayList<Request> request){
         this.request=request;
         this.context=context;
     }
 
-    public MyRequestAdapter(Context context, int activity_single_request, ArrayList<Request> request) {
+    public MyRequestAdapter1(Context context, int activity_single_his, ArrayList<Request> request) {
 
         this.request=request;
         this.context=context;
-        this.layoutResourseId=activity_single_request;
+        this.layoutResourseId=activity_single_his;
 
 
     }
@@ -284,7 +134,7 @@ class MyRequestAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.activity_single_request, null);
+        View view = LayoutInflater.from(context).inflate(R.layout.single_his, null);
         TextView eventType=(TextView) view.findViewById(R.id.EventType1);
         TextView status=(TextView) view.findViewById(R.id.status1);
         ImageView icon = view.findViewById(R.id.reqType1);
@@ -324,8 +174,5 @@ class MyRequestAdapter extends BaseAdapter{
         return view;
     }
 }
-
-
-
 
 
